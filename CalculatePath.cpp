8da67e8ab984c4421e92912321ba3e723ad358eb
jiangjing <jiangjing0824@thundersoft.com>
2019-11-06 20:57:34 +0800

    update

--- CalculatePath.cpp
+++ CalculatePath.cpp
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <iostream>
 
 using namespace std;
 
@@ -17,6 +18,7 @@ enum DIRECTION {
     LEFT,
     RIGHT
 };
+
 
 CalculatePath::CalculatePath(int sockfd): m_socketfd(sockfd) {
     startThread();
@@ -53,12 +55,17 @@ void CalculatePath::runLoop() {
     }
 }
 
-void CalculatePath::dispatch(char* msg, int size) {
+int CalculatePath::dispatch(char* msg, int size) {
+    if ((size != 227) || (msg[0] != '[') || (msg[226] != ']')) {
+        printf("msg %s is invaild! size = %d\n", msg, size);
+        return -1;
+    }
     char* recv_msg = (char*)malloc(size);
-    memcpy(recv_msg, msg, size);
+    memcpy(recv_msg, msg+1, size-2);
     unique_lock<mutex> lck(m_queue_mtx);
     m_queue.push(recv_msg);
     m_queue_cv.notify_one();
+	return 0;
 }
 
 char* CalculatePath::fetchNextMessage() {
@@ -82,51 +89,178 @@ int CalculatePath::sendMsg(char* msg) {
 
 void CalculatePath::handleMessage(char* msg) {
     //TODO: calculate path
-#if 0   /* only for test */
-    char result[3];
-    sprintf(result, "%s", "[d]");
-    sendMsg(result);
-#endif
+	char result[3];
+	int dir = 0;
+	static int count= 0;
     parseMapDate(msg);
+	dir = NextMove(msg);
+	switch(dir){
+		case UP:
+			sprintf(result, "%s", "[w]");
+			break;
+		case DOWN:
+			sprintf(result, "%s", "[s]");
+			break;
+		case LEFT:
+			sprintf(result, "%s", "[a]");
+			break;
+		case RIGHT:
+			sprintf(result, "%s", "[d]");
+			break;
+		default:
+			sprintf(result, "%s", "[d]");
+			break;
+	}
+	cout<<"!!!!!!!!!! count:"<<count<<"   move:"<<dir<<endl;
+	count ++;
+	sendMsg(result);
+
     free(msg);
 }
 
 int CalculatePath::parseMapDate(char* msg) {
     int size = strlen(msg);
-    if ((size != 227) || (msg[0] != '[') || (msg[226] != ']')) {
-        printf("msg %s is invaild! size = %d\n", msg, size);
-        return -1;
-    }
 
     int wall_count = 0;
     int ghost_count = 0;
+
+	cout<<"msg size:"<<endl;
     for(int i=0; i<size; i++) {
         if (msg[i] == '9') {  // wall
-            m_wall[wall_count] = i;
-            printf("wall count is [%d], position is [%d]\n", wall_count, i);
-            wall_count++;
+        	positionSet(&m_wall[wall_count],i);
+			//cout<<"wall pos["<<m_wall[wall_count].x+1<<","<<m_wall[wall_count].y+1<<"]"<<endl;
+			wall_count++;
         } else if (msg[i] == 'G') {  // ghost
-            m_ghost[ghost_count] = i;
-            printf("ghost count is [%d], position is [%d]\n", ghost_count, i);
+			positionSet(&m_ghost[ghost_count],i);
             ghost_count++;
         } else if (msg[i] == 'w') {  // player up
             m_player_d = UP;
-            m_player_p = i;
-            printf("player position is [%d]\n", i);
+			_m_player_p = i;
+			positionSet(&m_player_p,i);
         } else if (msg[i] == 'a') {  // player left
             m_player_d = LEFT;
-            m_player_p = i;
-            printf("player position is [%d]\n", i);
+			_m_player_p = i;
+			positionSet(&m_player_p,i);
         } else if (msg[i] == 's') {  // player down
             m_player_d = DOWN;
-            m_player_p = i;
-            printf("player position is [%d]\n", i);
+			_m_player_p = i;
+			positionSet(&m_player_p,i);
         } else if (msg[i] == 'd') {  // player right
             m_player_d = RIGHT;
-            m_player_p = i;
-            printf("player position is [%d]\n", i);
+			_m_player_p = i;
+			positionSet(&m_player_p,i);
         }
+		//cout<<"my pos["<<m_player_p.x+1<<","<<m_player_p.y+1<<"]"<<endl;
     }
     return 0;
 }
+
+void CalculatePath::positionSet(Pos* buff,int ID) {
+	buff->x=int(ID/15);
+	buff->y=int(ID%15);
+}
+
+
+int CalculatePath::NextMove(char* msg) {
+	int result = RIGHT;
+	int tempobject = 0;
+	int tempmove = RIGHT;
+	int a_object = 0xFF;
+	int s_object = 0xFF;
+	int d_object = 0xFF;
+	int w_object = 0xFF;
+	
+	a_object = IsAvailableObject(getPositionObject_Toplayer( 0, -1 ,msg ));
+	s_object = IsAvailableObject(getPositionObject_Toplayer( 1, 0, msg ));
+	d_object = IsAvailableObject(getPositionObject_Toplayer( 0, 1, msg ));
+	w_object = IsAvailableObject(getPositionObject_Toplayer( -1, 0, msg ));
+
+	cout<<"   a_object:"<<a_object<<"   s_object:"<<s_object<<"   d_object:"<<d_object<<"   w_object:"<<w_object<<endl;
+	if((a_object!=0xFF)&&(a_object>=tempobject))
+	{
+		tempmove = LEFT;
+		tempobject = a_object;
+
+	}
+	if((s_object!=0xFF)&&(s_object>=tempobject))
+	{
+		tempmove = DOWN;
+
+		tempobject = s_object;
+	}
+	if((d_object!=0xFF)&&(d_object>=tempobject))
+	{
+		tempmove = RIGHT;
+		tempobject = d_object;
+	}
+	if((w_object!=0xFF)&&(w_object>=tempobject))
+	{
+		tempmove = UP;
+		tempobject = w_object;
+	}
+	result = tempmove;
+	return	result;
+}
+#if 0
+Pos CalculatePath::findHighestScore(char* msg){
+
+	Pos tempPos;
+	Pos findPos;
+	int tempScore = 0;
+	int findScore = 0;
+	int radius = 1;
+	int i,j = 0;
+	bool findflag = false;
+	for( i=-radius; i<=radius; i++ )
+	{
+		for( j=-radius; j<=radius; j++ )
+		{
+			if((( m_player_p.x+ i >= 14 )&& ( m_player_p.x + i <= 14 ))
+				&&(( m_player_p.y+ j >= 14 )&& ( m_player_p.y + j <= 14 )))
+			{
+				//get position object
+				tempScore = atoi(msg[( m_player_p.x + i ) * 15 + ( m_player_p.y + j )]);
+				if((tempScore >= 0) && (tempScore <= 5) )
+				{
+					findflag = true;
+					if(findScore >= tempScore)
+					{
+						findPos.x = m_player_p.x + i;
+						findPos.y = m_player_p.y + j;
+						findScore = tempScore;
+					}
+ 
+				}
+			}
+		}
+	}
+
+}
+#endif
+
+
+char CalculatePath::getPositionObject_Toplayer(int x, int y, char* msg)
+{
+	char result = ' ';
+	int _pos = 0;
+	if(((m_player_p.x + x)>=0&&(m_player_p.x + x)<=14)
+		&&((m_player_p.y + y)>=0&&(m_player_p.y + y)<=14))
+	{
+		_pos = (m_player_p.x + x)*15+(m_player_p.y + y);
+		result = msg[_pos];
+	}
+	return result;
+}
+
+
+int CalculatePath::IsAvailableObject(char object)
+{
+	int result = 0xFF;
+	if((object>=0x30)&&(object<=0x35))
+	{
+		result = object -0x30;
+	}
+	return result;
+}
+
 
--- CalculatePath.h
+++ CalculatePath.h
@@ -7,12 +7,26 @@
 #include <mutex>
 #include <condition_variable>
 
+#if 1
+typedef struct _position
+{
+	int x;
+	int y;
+}Pos;
+#else
+struct Pos
+{
+	int x;
+	int y;
+
+}
+#endif
 class CalculatePath {
 public:
     CalculatePath(int sockfd);
     ~CalculatePath();
 
-    void dispatch(char* msg, int size);
+    int dispatch(char* msg, int size);
 
 private:
     int startThread();
@@ -24,6 +38,20 @@ private:
     int sendMsg(char* msg);
 
     int parseMapDate(char* msg);
+	void positionSet(Pos* buff,int ID);
+	int NextMove(char* msg);
+#if 0
+	Pos findHighestScore(char* msg);//3*3
+#endif
+	char getPositionObject_Toplayer(int x, int y, char* msg);
+	char getDirObjectA(char* msg);
+	char getDirObjectS(char* msg);
+	char getDirObjectD(char* msg);
+	char getDirObjectW(char* msg);
+	
+
+	int IsAvailableObject(char object);
+	
 
 private:
     int                      m_socketfd;
@@ -32,10 +60,14 @@ private:
     std::mutex               m_queue_mtx;
     std::condition_variable  m_queue_cv;
 
-    int  m_ghost[3];     // ghost position
-    int  m_player_p;     // player position
+    //int  m_ghost[3];     // ghost position
+    Pos m_ghost[3];
+	Pos m_player_p;
+	Pos m_wall[30];
+	Pos m_nextTarget;
+    int  _m_player_p;     // player position
     int  m_player_d;     // player direction
-    int  m_wall[30];     // wall position
+    //int  m_wall[30];     // wall position
 };
 
 #endif
